2c11e501034424609eb84ba1458f67a86234e49e
Jado <jbouiges@gmail.com>
2022-10-27 16:29:31 +0200

    suite projet flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,34 +11,69 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Conférence 2022"),
-        ),
-        body: Center(
-          child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("assets/images/flutter.png"),
-                const Text("Conférence 2022",
-                  style: TextStyle(
-                    fontSize: 42,
-                    fontFamily: 'Poppins'
-                  ),
-                ),
-                const Text("salon virtuel 2022",
-                    style:TextStyle(
-                    fontSize:24
-                ),
-            textAlign: TextAlign.center,
-                ),
+      home: HomePage(),
+    );
+  }
+}
 
-              ],
-          )
+class EventPage extends StatelessWidget {
+  const EventPage({Key? key}) : super(key: key);
 
-
-        )
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Planning du salon"),
       ),
+      body: Center(
+        child: Text("Prochainement disponible"),
+      )
+    );
+  }
+}
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Conférence 2022"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset("assets/images/logo.svg", color: Colors.blue),
+              const Text("Conférence 2022",
+                style: TextStyle(
+                  fontSize: 42,
+                  fontFamily: 'Poppins'
+                ),
+              ),
+              const Text("salon virtuel 2022",
+                  style:TextStyle(
+                  fontSize:24
+              ),
+          textAlign: TextAlign.center,
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                // backgroundColor: MaterialStatePropertyAll()
+              ),
+                onPressed: () => print("poyo"), child: Text("Afficher le planning",
+              style:TextStyle(
+                fontSize: 20
+              ),
+              ),
+              )
+            ],
+        )
+      )
     );
   }
 }

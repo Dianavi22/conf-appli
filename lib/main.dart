a223a3255d2084ea1132ea1f69a2e44e3f85b997
Jado <jbouiges@gmail.com>
2022-11-14 15:56:07 +0100

    ajout button et list

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import 'Pages/Home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,65 +18,5 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class EventPage extends StatelessWidget {
-  const EventPage({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Planning du salon"),
-      ),
-      body: Center(
-        child: Text("Prochainement disponible"),
-      )
-    );
-  }
-}
-
-
-class HomePage extends StatelessWidget {
-  const HomePage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Conférence 2022"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset("assets/images/logo.svg", color: Colors.blue),
-              const Text("Conférence 2022",
-                style: TextStyle(
-                  fontSize: 42,
-                  fontFamily: 'Poppins'
-                ),
-              ),
-              const Text("salon virtuel 2022",
-                  style:TextStyle(
-                  fontSize:24
-              ),
-          textAlign: TextAlign.center,
-              ),
-              ElevatedButton(
-                style: ButtonStyle(
-                // backgroundColor: MaterialStatePropertyAll()
-              ),
-                onPressed: () => print("poyo"), child: Text("Afficher le planning",
-              style:TextStyle(
-                fontSize: 20
-              ),
-              ),
-              )
-            ],
-        )
-      )
-    );
-  }
-}
 
